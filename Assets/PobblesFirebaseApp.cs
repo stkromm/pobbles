30d4b0bd87c211e343bf01044c9ba9ca8003741d
Steffen Kromm <kromm.steffen@gmail.com>
2018-10-31 15:37:30 +0100

    feat Added firebase integration fully for android and partially for ios

---
+++ Assets/PobblesFirebaseApp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class PobblesFirebaseApp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        FirebaseApp app = FirebaseApp.DefaultInstance;
+        FirebaseApp.LogLevel = LogLevel.Verbose;
+        FirebaseDatabase.DefaultInstance.LogLevel = LogLevel.Verbose;
+        // NOTE: You'll need to replace this url with your Firebase App's database
+        // path in order for the database connection to work correctly in editor.
+        app.SetEditorDatabaseUrl("https://pobbles-dev.firebaseio.com");
+        if (app.Options.DatabaseUrl != null)
+            app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        reference.Child("Test").SetValueAsync("Hello World!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
