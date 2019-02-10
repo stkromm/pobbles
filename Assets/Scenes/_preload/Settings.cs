5740be2232edd6704354890b126810b37434d95b
Lord-of-Milk <philipp.mittmann@web.de>
2019-02-10 14:56:34 +0100

    First Version of multiple language support via xml file.

--- Assets/Scenes/_preload/Settings.cs
+++ Assets/Scenes/_preload/Settings.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Settings : MonoBehaviour {
     private bool introBool;
-    
+    private string language;
+    private Lang langObject;
 
     private void Awake()
+    {
+        LoadIntroBool();
+        LoadLanguage();
+    }
+
+    //******Introduction************
+
+    private void LoadIntroBool()
     {
         if (PlayerPrefs.HasKey("introBool"))
         {
@@ -43,4 +52,35 @@ public class Settings : MonoBehaviour {
         }
     }
 
+    //********language support**********
+    private void LoadLanguage()
+    {
+        if (PlayerPrefs.HasKey("language"))
+        {
+            // if so, load the stored value
+            this.language = PlayerPrefs.GetString("language");
+        }
+        else
+        {
+            //else set to default system language
+            this.language = Application.systemLanguage.ToString();
+        }
+        //language object initialized with current language as Hashtable
+        langObject = new Lang(System.IO.Path.Combine(Application.dataPath, "Lang.xml"), this.language, false);
+    }
+
+    public void SetLanguage(string language)
+    {
+        PlayerPrefs.SetString("language", language);
+    }
+
+    public string GetLanguage()
+    {
+        return this.language;
+    }
+
+    public string GetStringFromHashtable(string key)
+    {
+        return langObject.getString(key);
+    }
 }
---
+++ Assets/Translator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Translator : MonoBehaviour
+{
+    private Settings settingsObject;
+    private string currentLang;
+    Text[] texts;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        settingsObject = GameObject.FindObjectOfType<Settings>();
+        Translate(settingsObject.GetLanguage());
+    }
+    
+    private void Translate(string language)
+    {
+        currentLang = language;
+        Debug.Log("the langauge is : " + language);
+        texts = gameObject.GetComponentsInChildren<Text>();
+        foreach (Text text in texts){
+            //only change the text, if there is a value stored in the hashtable
+            Debug.Log("current key: " + text.name);
+            Debug.Log("passed value: " + settingsObject.GetStringFromHashtable(text.name));
+            if (settingsObject.GetStringFromHashtable(text.name)!="")
+            {
+                text.text = settingsObject.GetStringFromHashtable(text.name);
+            }
+            
+        }
+        
+    }
+    
+
+    private void Update()
+    {
+        //check for language changes and update if language has changed
+        if (currentLang != settingsObject.GetLanguage())
+        {
+            Translate(settingsObject.GetLanguage());
+        }
+    }
+}
