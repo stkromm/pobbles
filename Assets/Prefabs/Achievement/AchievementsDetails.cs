d3b7649ff7582a36c24a282633b6c02857fc3fa8
Lord-of-Milk <philipp.mittmann@web.de>
2019-05-13 22:58:04 +0200

    Updated the achievement prefab. first achievement init routine implemented.

---
+++ Assets/Prefabs/Achievement/AchievementsDetails.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementsDetails : MonoBehaviour
+{
+    public Image achievementIcon;
+    public Text achievementHeading;
+    public Text achievementDescription;
+    public Slider achievementProgressSlider;
+
+    private void Start()
+    {
+        InitAchievement("baseline_chat_white_48dp", "test name", "beschreibung");
+    }
+
+    //called upon initializing
+    public void InitAchievement(string iconName, string heading, string description)
+    {
+        //load icon
+        Debug.Log("AchievementIcons/"+iconName);
+        achievementIcon.sprite = Resources.Load<Sprite>("AchievementIcons/" + iconName);
+        //change heading
+        achievementHeading.text = heading;
+        //change description
+        achievementDescription.text = description;
+        //update progress
+        LoadAchievementProgress(heading);
+    }
+    
+    private void LoadAchievementProgress(string key)
+    {
+        //load progress from local db or server
+        float progress = 0.5f;
+
+        //adjust the progress bar
+        achievementProgressSlider.value = progress;
+    }
+    
+
+
+    }
--- Assets/Scenes/Achievements/Achievements.cs
+++ Assets/Scenes/Achievements/Achievements.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Achievements : MonoBehaviour
 {
+    public GameObject Achievement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
