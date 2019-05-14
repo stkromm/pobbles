748839fc3d73f9c6b448554e55f283379d9df9e9
Lord-of-Milk <philipp.mittmann@web.de>
2019-05-14 22:36:32 +0200

    Achievement Prefab finished. Dynamic List Creation with scroll rect implemented. Achievement Init finised.

---
+++ Assets/Prefabs/Achievement/Achievement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievement
+{
+    public Sprite achievementIcon;
+    public string achievementHeading;
+    public string achievementDescription;
+    public float achievementProgressSlider;
+
+    public Achievement(Sprite icon, string heading, string description, float progress)
+    {
+        achievementIcon = icon;
+        achievementHeading = heading;
+        achievementDescription = description;
+        achievementProgressSlider = progress;
+    }
+}
---
+++ Assets/Scenes/Achievements/AchievementListController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementListController : MonoBehaviour
+{
+    ArrayList achievements;
+    public GameObject content;
+    public GameObject achievementListItem;
+
+    void Start()
+    {
+
+        //Get the data to be displayed
+        achievements = new ArrayList(){
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.3f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.5f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.7f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.7f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.7f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.7f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.7f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.7f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.7f ),
+             new Achievement(Resources.Load<Sprite>("AchievementIcons/testtest"), "Test", "Test beschr.",0.9f )
+        };
+
+        // 2. Iterate through the data, 
+        //	  instantiate prefab, 
+        //	  set the data, 
+        //	  add it to panel
+        foreach (Achievement achievement in achievements)
+        {
+            GameObject newAchievement = Instantiate(achievementListItem) as GameObject;
+            AchievementController controller = newAchievement.GetComponent<AchievementController>();
+            controller.achievementIcon.sprite = achievement.achievementIcon;
+            controller.achievementHeading.text = achievement.achievementHeading;
+            controller.achievementDescription.text = achievement.achievementDescription;
+            controller.achievementProgressSlider.value = achievement.achievementProgressSlider;
+
+            newAchievement.transform.parent = content.transform;
+            newAchievement.transform.localScale = Vector3.one;
+        }
+    }
+}
