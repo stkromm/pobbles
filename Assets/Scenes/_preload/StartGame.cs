744c1857b2a7be5a1926a1049f5389a20d51d3a3
Steffen Kromm <kromm.steffen@gmail.com>
2019-06-11 18:35:40 +0200

    Added RequireComp and simplified script

--- Assets/Scenes/_preload/StartGame.cs
+++ Assets/Scenes/_preload/StartGame.cs
@@ -3,23 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof (Settings))]
 public class StartGame : MonoBehaviour {
-    private Settings settingsObject;
-
-	// Use this for initialization
+    Settings settingsObject;
+    
 	void Start () {
-        this.settingsObject = Object.FindObjectOfType<Settings>();
+        settingsObject = GetComponent<Settings>();
         SceneManager.LoadScene("MainMenu");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 
     public void PlayGame()
     {
-        Debug.Log("play Game called");
         if (settingsObject.GetIntroBool())
         {
             SceneManager.LoadScene("Introduction");
