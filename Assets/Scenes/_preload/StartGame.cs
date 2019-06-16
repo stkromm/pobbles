d07cbded32c15a9e6c40f2886057d00678c207a9
Steffen Kromm <kromm.steffen@gmail.com>
2019-06-16 16:50:52 +0200

    load firebase in preloadd

--- Assets/Scenes/_preload/StartGame.cs
+++ Assets/Scenes/_preload/StartGame.cs
@@ -1,16 +1,63 @@
-﻿using System.Collections;
+﻿using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof (Settings))]
+[RequireComponent(typeof (Settings)), RequireComponent(typeof (DDOL)), RequireComponent(typeof (PobblesFirebaseApp))]
 public class StartGame : MonoBehaviour {
     Settings settingsObject;
+    PobblesFirebaseApp firebaseApp;
+    const string FIREBASE_URL = "https://pobbles-dev.firebaseio.com";
+    bool init = false;
     
-	void Start () {
+    void Start ()
+    {
         settingsObject = GetComponent<Settings>();
-        SceneManager.LoadScene("MainMenu");
-	}
+        firebaseApp = GetComponent<PobblesFirebaseApp>();
+        Init();
+    }
+
+    async void Init()
+    {
+            FirebaseApp app = FirebaseApp.DefaultInstance;
+            FirebaseApp.LogLevel = LogLevel.Verbose;
+            FirebaseDatabase.DefaultInstance.LogLevel = LogLevel.Verbose;
+            app.SetEditorDatabaseUrl(FIREBASE_URL);
+            if (app.Options.DatabaseUrl != null)
+                app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+            FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+            if (auth.CurrentUser == null)
+            {
+                try
+                {
+                    var user = await auth.SignInAnonymouslyAsync();
+
+                    Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    user.DisplayName, user.UserId);
+                    init = true;
+                }
+                catch (Exception e)
+                {
+                    init = true;
+
+                }
+        }
+        init = true;
+    }
+
+    void Update()
+    {
+        if (init)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }        
+    }
 
     public void PlayGame()
     {
--- Assets/Scripts/PobblesFirebaseApp.cs
+++ Assets/Scripts/PobblesFirebaseApp.cs
@@ -5,44 +5,18 @@ using Firebase;
 using Firebase.Database;
 using Firebase.Auth;
 using Firebase.Unity.Editor;
+using System.Threading.Tasks;
+using System;
 
-public class PobblesFirebaseApp : MonoBehaviour {
+public class PobblesFirebaseApp : MonoBehaviour { 
+    private bool isInitialized = false;
 
-
-	// Use this for initialization
-	void Start () {
-        FirebaseApp app = FirebaseApp.DefaultInstance;
-        FirebaseApp.LogLevel = LogLevel.Verbose;
-        FirebaseDatabase.DefaultInstance.LogLevel = LogLevel.Verbose;
-        // NOTE: You'll need to replace this url with your Firebase App's database
-        // path in order for the database connection to work correctly in editor.
-        app.SetEditorDatabaseUrl("https://pobbles-dev.firebaseio.com");
-        if (app.Options.DatabaseUrl != null)
-            app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
-        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-        if (auth.CurrentUser == null){
-            auth.SignInAnonymouslyAsync().ContinueWith(task => {
-                if (task.IsCanceled)
-                {
-                    Debug.LogError("SignInAnonymouslyAsync was canceled.");
-                    return;
-                }
-                if (task.IsFaulted)
-                {
-                    Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
-                    return;
-                }
-
-                Firebase.Auth.FirebaseUser newUser = task.Result;
-                Debug.LogFormat("User signed in successfully: {0} ({1})",
-                    newUser.DisplayName, newUser.UserId);
-
-            });
-        }
+    void Start()
+    {
+        
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    async public Task Init() {
+       
+    }
 }
