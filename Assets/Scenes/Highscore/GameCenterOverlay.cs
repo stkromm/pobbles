1fd503b88038d1a82c9c88526c555e42a0743066
Lord-of-Milk <philipp.mittmann@web.de>
2019-08-15 18:17:04 +0200

    implemented basic pop up for game center set up handling on iOS

---
+++ Assets/Scenes/Highscore/GameCenterOverlay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameCenterOverlay : MonoBehaviour
+{
+
+    public Button gameCenterSetUpButton;
+    public Button gameCenterBackButton;
+    public Button socialSigninButton;
+    public GameObject gameCenterOverlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Script on disabled object called.");
+        socialSigninButton.onClick.AddListener(delegate
+        {
+            gameCenterOverlay.SetActive(true);
+            Debug.Log("Button pressed");
+        });
+
+        gameCenterSetUpButton.onClick.AddListener(delegate
+        {
+            gameCenterOverlay.SetActive(false);
+            LinkToIOSSettings();
+        });
+        gameCenterBackButton.onClick.AddListener(delegate
+        {
+            gameCenterOverlay.SetActive(false);
+        });
+    }
+
+    void LinkToIOSSettings()
+    {
+        //open iOS game center settings
+    }
+}
