b13eee4beecd110b6c975bc6b72a4b9e0ae30382
Lord-of-Milk <philipp.mittmann@web.de>
2019-10-20 10:20:13 +0200

    Split into Arcade and Classic Mode. Animated Transition between mods.

--- Assets/Scenes/ClassicLevel/ClassicLevel.cs
+++ Assets/Scenes/ClassicLevel/ClassicLevel.cs
@@ -6,31 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class ClassicLevel : MonoBehaviour
 {
-    public Text usernameText;
-    public Text userScoreText;
-    public Text levelText;
     private StartGame startGameObject;
+
     // Holds information about which level is being displayed at the moment
     private int currentLevel = 1;
     // Buttons
     public Button playGameButton;
-    public Button nextLevelButton;
-    public Button previousLevelButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        if(currentLevel == 1)
-        {
-            HidePrevLevelButton();
-        }
-        // Get username and set usernameText
-        // Get users current Score/Rank/Level and set userScoreText
-        // Get current level, set levelText/currentLevel to current level
-        nextLevelButton.onClick.AddListener(ShowNextLevel);
-        previousLevelButton.onClick.AddListener(ShowPreviousLevel);
+
         startGameObject = Object.FindObjectOfType<StartGame>();
         playGameButton.onClick.AddListener(delegate
         {
+            Debug.Log("PlayGame Pressed");
             startGameObject.PlayGame();
         });
     }
@@ -41,19 +31,5 @@ public class ClassicLevel : MonoBehaviour
         
     }
     
-    void HidePrevLevelButton()
-    {
-        previousLevelButton.gameObject.SetActive(false);
-    }
-
-    void ShowNextLevel()
-    {
-        // Add animation to slide over to next level
-    }
-
-    void ShowPreviousLevel()
-    {
-        // Add animation to slide back to previous level
-    }
 
 }
